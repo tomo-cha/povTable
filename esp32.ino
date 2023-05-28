d0e1be130b69bdd2285046e90846895ea9cab689
takahara tomoaki <jihyun8286@gmail.com>
2023-05-28 16:48:02 +0900

    一つのspi

---
+++ esp32.ino
@@ -0,0 +1,93 @@
+#include "Adafruit_DotStar.h"
+#include <SPI.h>
+#include "graphics.h"
+
+
+int numRot = 0;
+int numDiv = 0;
+int stateDiv = 0;
+int stateRot = 0;
+const PHOTOPIN = 33;
+const th_SENSORS = 3800; //赤外線センサの閾値
+float rpm;
+volatile unsigned long rotTime, timeOld, timeNow; //コンパイル時に無視されないようにvolatile修飾子
+const VDATAPIN = 23;
+const VCLOCKPIN = 18;
+// const HDATAPIN = 13;
+// const HCLOCKPIN = 14;
+
+
+Adafruit_DotStar vstrip(VNUMPIXELS, VDATAPIN, VCLOCKPIN, DOTSTATR_RGB); //DOTSTATR_BRGなどでも設定可能
+// Adafruit_DotStar hstrip(HNUMPIXELS, HDATAPIN, HCLOCKPIN, DOTSTATR_RGB);
+
+
+void setup()
+{
+    Serial.begin(115200);
+
+    vstrip.begin();
+    htrip.begin();
+    vstrip.show();
+    // hstrip.show();
+}
+
+
+void void loop()
+{
+    Serial.print(analogRead(PHOTOPIN));//赤外線センサの動作確認用
+
+
+    //赤外線センサの排他処理, 一回転(赤外線センサが反応してから次に反応するまで)するのにかかる時間(rotTime)の計算
+    if(stateRot == 0 && analogRead(PHOTOPIN) < th_SENSORS)
+    {
+        timeNow = micros();
+        rotTime = timeNow - timeOld;
+        timeOld = timeNow;
+        stateRot = 1;
+    }
+    if(stateRot == 1 && analogRead(PHOTOPIN) > th_SENSORS)
+    {
+        stateRot = 0;
+        //rpm = 60 * 1000 * 1000 * 1 / rotTime; //rotation per minute
+        //Serial.print("rpm="); Serial.println(rpm);
+    }
+
+
+    if(stateDiv == 1 && micros() -timeOld > rotTime / Div * numDiv)
+    {
+        stateDiv = 0;
+    }
+    if(stateDiv == 1 && micros() -timeOld < rotTime / Div * numDiv)
+    {
+        stateDiv = 1;
+
+
+        vstrip.clear();
+        // hstrip.clear();
+
+
+        //どのLEDを何色に光らせるのかをset
+        for(int i=0; i<VNUMPIXELS; i++)
+        {
+            vstrip.setPixelColor(i, pgm_read_dword(&vpic[numDiv][i]));
+        }
+        // for(int i=0; i<HNUMPIXELS; i++)
+        // {
+        //     hstrip.setPixelColor(i, pgm_read_dword(&hpic[numDiv+(Div/2)][i])); //半分ずれる
+        // }
+
+        
+        //setした通りに光らせる
+        vstrip.show();
+        // hstrip.show();
+
+
+        numDiv++;
+        if(numDiv >= Div)
+        {
+            numDiv = 0;
+        }
+    }
+}
+
+
---
+++ pic_polarConv.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+# 画像一枚を1つのファイルに変換するコード
+import cv2
+import os
+import math
+import sys
+from PIL import Image
+
+
+args = sys.argv
+
+# 配列設定
+PIXELS = 24  #LED1本あたりのセル数
+NUMTAPES = 2    #繋げるLEDの本数
+Div = 100       #1周の分割数
+l = [[0] * PIXELS*NUMTAPES for i in range(Div)] #LEDsのRGBを格納するためのリスト宣言・初期化
+
+Bright = 20     # 輝度
+Led0Bright = 2  # 中心LEDの輝度 [%]
+
+# ファイル作成
+file = open('graphics.h', 'w')
+file.write('const NUMPIXELS ' + str(PIXELS*NUMTAPES) + '\n')
+file.write('const Div ' + str(Div) + '\n' + '\n')
+
+
+file.write('const uint32_t pic [Div][NUMPIXELS] = {' + '\n')
+
+# 画像ファイルを読み込む(png,jpg,bmpなどが使用可能)
+pic1 = "monstar.png"
+pic2 = "monstar.png"
+
+
+# 画像変換関数
+def polarConv(pic, n):
+    # 画像データ読み込み
+    imgOrgin = cv2.imread(pic)
+
+    # 画像サイズ取得
+    h, w, _ = imgOrgin.shape
+
+    # 画像縮小
+    # 画像のh:画像のw = (PIXELS * 2 -1):?. の比を取って縮小してる
+    imgRedu = cv2.resize(imgOrgin, (math.floor(
+        (PIXELS * 2 - 1)/h * w), PIXELS * 2 - 1))
+
+    # 縮小画像中心座標
+    h2, w2, _ = imgRedu.shape
+    wC = math.floor(w2 / 2)
+    hC = math.floor(h2 / 2)
+
+    # 極座標変換画像準備
+    # 第二引数にサイズ、第三引数にRGBの各色を指定する。
+    # imgPolar = Image.new('RGB', (PIXELS, Div))
+
+    # 極座標変換
+    for j in range(0, Div):
+        # file.write('\t{')
+        for i in range(0, hC+1):
+            # 座標色取得
+            # 参考：http://peaceandhilightandpython.hatenablog.com/entry/2016/01/03/151320
+            rP = int(imgRedu[hC + math.ceil(i * math.cos(2*math.pi/Div*j)),
+                             wC - math.ceil(i * math.sin(2*math.pi/Div*j)), 2]  # Rを取得
+                     * ((100 - Led0Bright) / PIXELS * i + Led0Bright) / 100 * Bright / 100)  # 明るさ調整
+            gP = int(imgRedu[hC + math.ceil(i * math.cos(2*math.pi/Div*j)),
+                             wC - math.ceil(i * math.sin(2*math.pi/Div*j)), 1]  # Gを取得
+                     * ((100 - Led0Bright) / PIXELS * i + Led0Bright) / 100 * Bright / 100)  # 明るさ調整
+            bP = int(imgRedu[hC + math.ceil(i * math.cos(2*math.pi/Div*j)),
+                             wC - math.ceil(i * math.sin(2*math.pi/Div*j)), 0]  # Bを取得
+                     * ((100 - Led0Bright) / PIXELS * i + Led0Bright) / 100 * Bright / 100)  # 明るさ調整
+            
+            # https://yutarine.blogspot.com/2018/12/python-format-hex-zerofill.html
+            # https://docs.python.org/ja/3/tutorial/inputoutput.html
+            if(n%2 == 1):
+                l[j][i] = '0x{:02X}{:02X}{:02X}'.format(rP, gP, bP)
+            else:
+                l[j][abs((PIXELS*n-1)-i)] = '0x{:02X}{:02X}{:02X}'.format(rP, gP, bP)
+
+            # imgPolar.putpixel((i, j), (rP, gP, bP))
+
+# 変換, lに格納
+polarConv(pic1, 1) #0~23番目のセル l[j][i]
+polarConv(pic2, 2) #47~24番目のセル l[j][47-i]
+
+#lの内容を書き込む
+for j in range(0, Div):
+    file.write('\t{')
+    for i in range(0, PIXELS*NUMTAPES):
+        file.write(l[j][i])
+        if i == PIXELS*NUMTAPES-1:
+            file.write('},\n')
+        else:
+            file.write(', ')
+file.write('};\n')
+
+
+file.close()
